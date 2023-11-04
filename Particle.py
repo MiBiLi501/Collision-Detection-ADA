520cc196d36c7fae9b8dce3ea5371111dca1fd07
Ëzo-Tanuki <michailbryanlie@gmail.com>
2023-11-04 13:27:46 +0700

    add collision

--- Particle.py
+++ Particle.py
@@ -1,6 +1,18 @@
 import math
 import pygame as pyg
 
+GRAVITY = None
+DRAG = 1
+ELASTICITY = 1
+
+def addVectors(angle1, length1, angle2, length2):
+    x = length1 * math.cos(angle1) + length2 * math.cos(angle2)
+    y = length1 * math.sin(angle1) + length2 * math.sin(angle2)
+    
+    length = math.hypot(x, y)
+    angle = math.atan2(y, x)
+    return (angle, length)
+
 class Particle():
     def __init__(self, screen, x, y, size):
         self.screen = screen
@@ -19,23 +31,31 @@ class Particle():
         if self.x > width - self.size:
             self.x = 2 * (width - self.size) - self.x
             self.angle = math.pi - self.angle
+            self.speed *= ELASTICITY
 
         elif self.x < self.size:
             self.x = 2 * self.size - self.x
             self.angle = math.pi - self.angle
+            self.speed *= ELASTICITY
 
         if self.y > height - self.size:
             self.y = 2 * (height - self.size) - self.y
             self.angle *= -1
+            self.speed *= ELASTICITY
         
         elif self.y < self.size:
             self.y = 2 * self.size - self.y
             self.angle *= -1
+            self.speed *= ELASTICITY
 
 
     def display(self):
         pyg.draw.circle(self.screen, self.color, (self.x, self.y), self.size, self.thickness)
     
     def move(self):
+        if GRAVITY is not None:
+            (self.angle, self.speed) = addVectors(self.angle, self.speed, *GRAVITY)
         self.x += math.cos(self.angle) * self.speed
         self.y += math.sin(self.angle) * self.speed
+        self.speed *= DRAG
+        
--- Window.py
+++ Window.py
@@ -4,7 +4,32 @@ import math
 import pygame as pyg
 import random
 
-WINDOW_SIZE = (600, 400)
+ELASTICITY = 1
+
+def collide(p1, p2):
+    dx = p1.x - p2.x
+    dy = p1.y - p2.y
+    
+    dist = math.hypot(dx, dy)
+    if dist < p1.size + p2.size:
+        tangent = -math.atan2(dy, dx)
+        angle = tangent
+
+        angle1 = 2 * (math.pi/2 - tangent) - p1.angle
+        angle2 = 2 * (math.pi/2 - tangent) - p2.angle
+        speed1 = p2.speed*ELASTICITY
+        speed2 = p1.speed*ELASTICITY
+
+        p1.angle, p1.speed = angle1, speed1
+        p2.angle, p2.speed = angle2, speed2
+
+        p1.x += math.cos(angle)
+        p1.y -= math.sin(angle)
+        p2.x -= math.cos(angle)
+        p2.y += math.sin(angle)
+
+
+WINDOW_SIZE = (1000, 400)
 WHITE = (255, 255, 255)
 
 screen = pyg.display.set_mode(WINDOW_SIZE)
@@ -13,7 +38,7 @@ screen.fill(WHITE)
 pyg.display.set_caption("Test")
 pyg.display.flip()
 
-number_of_particles = 10
+number_of_particles = 20
 my_particles = []
 for n in range(number_of_particles):
     size = random.randint(10, 20)
@@ -31,11 +56,12 @@ while running:
     for event in pyg.event.get():
         if event.type == pyg.QUIT:
             running = False
-    sie = random.randint(10, 20)
 
-    for particle in my_particles:
+    for i, particle in enumerate(my_particles):
         particle.move()
         particle.bounce()
+        for particle2 in my_particles[i+1:]:
+            collide(particle, particle2)
         particle.display()
 
     pyg.display.flip()
